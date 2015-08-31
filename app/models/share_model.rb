2ec65471e3ee59dc6daa28bdaf901b17e9025b01
Angel M de Miguel <angelmm@redborder.net>
2015-08-31 10:38:35 +0200

    Fixed the reference of model who share resource from sharer to shared_from

--- app/models/share_model.rb
+++ app/models/share_model.rb
@@ -9,9 +9,9 @@ class ShareModel < ActiveRecord::Base
   # Model with resource is shared
   belongs_to :shared_to, polymorphic: true
   # User who share resources
-  belongs_to :sharer, polymorphic: true
+  belongs_to :shared_from, polymorphic: true
 
-  validates :resource, :shared_to, :sharer, presence: true
+  validates :resource, :shared_to, :shared_from, presence: true
   # Every resource only can be shared with one person from same sharer one time.
   validates :resource_id, uniqueness: { scope: [:resource_type, :shared_to_id,
                                                 :shared_to_type] }
